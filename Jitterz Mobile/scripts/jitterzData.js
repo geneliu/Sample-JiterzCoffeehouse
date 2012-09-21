926c7b6e74deee22b416e8032473f8f824f0a8e1
Todd Anglin <toddanglin@gmail.com>
2012-09-21 13:27:34 -0700

    Added basic support for caching JSON data in localStorage

--- Jitterz Mobile/scripts/jitterzData.js
+++ Jitterz Mobile/scripts/jitterzData.js
@@ -14,14 +14,22 @@ var JitterzData = function(){
 
             console.log("GETTING", path, verb, options);
 
-            //TODO: Use cached data before re-querying (if fresh)
-            if (!dfd.isRejected()) {
+            //Return cached data if available (and fresh)
+            if(verb === "GET" && _private.checkCache(path) === true){
+                //Return cached data
+                dfd.resolve(_private.getCache(path));
+            }else{
+                //Get fresh data
                 $.ajax({
                     type: verb,
                     url: path,
                     data: options,
                     dataType: "json"
                 }).success(function (data, code, xhr) {
+                    _private.setCache(path, {
+                        data: data,
+                        expires: new Date(new Date().getTime() + (15*60000)) //+15min
+                    })
                     dfd.resolve(data, code, xhr);
                 }).error(function (e, r, m) {
                     console.log("ERROR", e, r, m);
@@ -30,6 +38,48 @@ var JitterzData = function(){
             }
 
             return dfd.promise();
+        },
+        checkCache: function(path){
+            var data,
+                path = JSON.stringify(path);
+
+            try{
+                data = JSON.parse(localStorage.getItem(path));
+                
+                if(data === null || data.expires <= new Date().getTime()) 
+                {
+                    console.log("CACHE EMPTY", path);
+                    return false;
+                }
+            }
+            catch(err){
+                console.log("CACHE CHECK ERROR", err);
+                return false;
+            }
+
+            console.log("CACHE CHECK", true, path);
+            return true;
+        },
+        setCache: function(path, data, expires){
+            var cache = {
+                    data: data,
+                    expires: expires
+                },
+                path = JSON.stringify(path);
+
+            //TODO: Serialize JSON object to string
+            localStorage.setItem(path, JSON.stringify(cache));
+
+            console.log("CACHE SET", cache, new Date(expires), path);
+        },
+        getCache: function(path){
+            var path = JSON.stringify(path),
+                cache = JSON.parse(localStorage.getItem(path));
+
+            console.log("LOADING FROM CACHE", cache, path);
+
+            //TODO: Deserialize JSON string
+            return cache.data.data;
         }
 	}
 
